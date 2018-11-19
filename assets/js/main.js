fee6277bea77e725f74e28d10e12aaace5a9ffe6
MariQerdika <40671485+MariQerdika@users.noreply.github.com>
2018-11-19 23:49:02 +0400

    animation text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,25 @@
+  $(document).ready(function(){
+  $('.toggle').click(function(){
+    $('.menu_list').toggleClass('active');
+  });
+});
+
   jQuery(document).ready(function(){
   "use strict"
   $('.slider').ripples({
     dropRadius: 15,
     pertuarbance: 0.01,
   });
+
+
+var typed = new Typed('.text', {
+  strings: ["Front-end Development", "Back-end development"],
+  typeSpeed: 60,
+  loop:true,
+});
+
 });
 
 
 
-  $(document).ready(function(){
-  $('.toggle').click(function(){
-    $('.menu_list').toggleClass('active');
-  });
-});
+  
